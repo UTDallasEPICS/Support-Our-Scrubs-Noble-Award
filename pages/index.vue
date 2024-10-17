81194c03ee95785b592260387ac763f06a80bd30
nxp-22 <nandinipaidesetty@gmail.com>
2024-10-17 16:38:48 -0500

    yippee

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <p>Hello</p>
+       <nuxt-link to="/nominee">
+        <button>Go to Nominee Page</button>
+      </nuxt-link> 
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'YourComponentName'
+  }
+  </script>
+  
+  <style scoped>
+  /* Add styles here if needed */
+  </style>
+  
---
+++ pages/nominee/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Nominee Page</h1>
+    <p>This is the nominee page.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NomineePage'
+}
+</script>
