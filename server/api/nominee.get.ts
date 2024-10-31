933c45e6cd40bd07aa0d7eeccac1da3f19fdfdb7
Lyn-ance <BNN230000@utdallas.edu>
2024-10-31 16:17:28 -0500

    working gets

--- server/api/nominee.get.ts
+++ server/api/nominee.get.ts
@@ -1,26 +1,41 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, Status } from "@prisma/client";
+import { getQuery } from 'h3';
+
 const prisma = new PrismaClient();
 
 export default defineEventHandler(async (event) => {
+    const query = getQuery(event);
+    const findId = typeof query.findId === 'string' ? query.findId : undefined;
+    const stat = typeof query.stat === 'string' ? query.stat : undefined;
 
-    const body = await readBody(event);
+    try {
+        let nominees;
+       
+        if (findId) {
+            nominees = await prisma.nominee.findUnique({
+                where: { id: findId },
+            });
+        } 
+        else if(stat)
+        {
+            if (Object.values(Status).includes(stat as Status)) {
+                nominees = await prisma.nominee.findMany({
+                    where: { status: stat as Status }, // Cast to Status enum
+                });
+            }
+        }
+        else if (!stat && !findId) {
+            nominees = await prisma.nominee.findMany();
+        }
 
-    const findId = body.findId;
-
-    try{
-        const nominee = await prisma.nominee.findUnique({
-            where: {
-            id: findId,
-            },
-        })
-
-        return nominee;
+        return nominees;
     }
-
-    catch(error) {
+    catch (error) {
         console.log(error);
-        throw createError({ statusCode: 500, statusMessage: "Error getting nominee", });
+        throw createError({
+            statusCode: 500,
+            statusMessage: "Error getting nominee",
+        });
     }
-
-
 });
+     
