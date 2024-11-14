1c29df23e8c38e499e2f2d55ca4fa363c8127989
AjileVenkat <86122369+AjileVenkat@users.noreply.github.com>
2024-11-14 15:53:00 -0600

    made dropdown menu black

--- pages/nominator/viewnominees.vue
+++ pages/nominator/viewnominees.vue
@@ -23,7 +23,7 @@
   
         <!-- Dropdown Menu -->
         <div
-          class="absolute right-0 z-10 mt-2 w-full origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+          class="absolute right-0 z-10 mt-2 w-full origin-top-right rounded-md bg-black shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
           role="menu"
           aria-orientation="vertical"
           aria-labelledby="menu-button"
