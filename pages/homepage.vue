5ddfa51d2e287e9daf58a5bcb86f12fbc421384f
sofiathomas715 <sofiathomas2901@gmail.com>
2024-11-21 15:17:20 -0600

    made form a component

---
+++ pages/homepage.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="min-h-screen flex justify-center items-center bg-black">
+      <FormComponent @onSubmit="handleFormSubmit" />
+    </div>
+  </template>
+  
+  <script>
+  import FormComponent from "@/components/FormComponent.vue";
+  
+  export default {
+    components: {
+      FormComponent,
+    },
+    methods: {
+      handleFormSubmit(formData) {
+        console.log("Form data received from component:", formData);
+        // Handle form data (e.g., save to database, call API)
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* Add additional styles if necessary */
+  </style>
+  
