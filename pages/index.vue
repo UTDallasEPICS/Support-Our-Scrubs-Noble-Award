fc7293a9fda226a7c9ca8ed152dd8d69dc53bc10
sofiathomas715 <sofiathomas2901@gmail.com>
2024-11-21 16:03:27 -0600

    made form a component

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,26 @@
 <template>
-  <div>
-    <p>Hello</p>
-     <nuxt-link to="/nominator">
-      <button>Go to Nominator Page</button>
-    </nuxt-link> 
-  </div>
+    <div class="min-h-screen flex justify-center items-center bg-black">
+      <FormComponent @onSubmit="handleFormSubmit" />
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'YourComponentName'
-}
-</script>
-
-<style scoped>
-
-</style>
+ import FormComponent from "@/components/FormComponent.vue";
+  
+  export default {
+    components: {
+      FormComponent,
+    },
+    methods: {
+      handleFormSubmit(formData) {
+        console.log("Form data received from component:", formData);
+        // Handle form data (e.g., save to database, call API)
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* Add additional styles if necessary */
+  </style>
+  
