b5207e7b5246cc32de4efa7940249ad5cee854f5
Aagam Shah <shahaagam04@gmail.com>
2026-02-26 11:05:05 -0600

    feat: add nuxt-security module and update package dependencies; modify nuxt.config.ts for security settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,47 +1,68 @@
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
-  imports: { autoImport: true },
-  modules: ['@nuxtjs/supabase'],
+    compatibilityDate: "2024-04-03",
+    devtools: { enabled: true },
+    css: ["~/assets/css/main.css"],
+    imports: { autoImport: true },
+    modules: ["@nuxtjs/supabase", "nuxt-security"],
 
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+        },
     },
-  },
 
-  runtimeConfig: {
-      DATABASE_URL: "file:./dev.db",
-	  supabase: {
-		  serviceKey: process.env.NUXT_SUPABASE_SERVICE_ROLE_KEY!,
-	  },
-      public:{
-        supabase: {
-          url: process.env.NUXT_PUBLIC_SUPABASE_URL!,
-          key: process.env.NUXT_PUBLIC_SUPABASE_ADON_KEY!,
-          redirectOptions: {
-            // we are NOT using a /login route
-            login: '/',                      // unused now, but required by your plugin’s shape
-            callback: '/auth/callback',
-            include: ['/admin/**'],  // only guard these
-            exclude: ['/', '/roster', '/donate', '/contact', '/auth/callback'],
-            cookieRedirect: false,
-            saveRedirectToCookie: false
-        }
+    security: {
+      corsHandler: {
+        origin: '*',
+        methods: ['GET', 'POST', 'PUT', 'DELETE']
       }
-    }
-  },
+    },
 
-  app: {
-   head: {
-     link: [
-       {
-         rel: 'stylesheet',
-         href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css'
-       }
-     ]
-   }
- },
+    runtimeConfig: {
+        DATABASE_URL: "file:./dev.db",
+
+        clientID: process.env.NUXT_OAUTH2_CLIENT_ID!,
+        clientSecret: process.env.NUXT_OAUTH2_CLIENT_SECRET!,
+        refreshToken: process.env.NUXT_OAUTH2_REFRESH_TOKEN!,
+        redirectURI: process.env.NUXT_OAUTH2_REDIRECT_URI!,
+        jwtSecret: process.env.JWT_SECRET,
+        supabase: {
+            serviceKey: process.env.NUXT_SUPABASE_SERVICE_ROLE_KEY!,
+        },
+        public: {
+            emailHost: process.env.NUXT_PUBLIC_EMAIL_SENDER!,
+            
+            supabase: {
+                url: process.env.NUXT_PUBLIC_SUPABASE_URL!,
+                key: process.env.NUXT_PUBLIC_SUPABASE_ADON_KEY!,
+                redirectOptions: {
+                    // we are NOT using a /login route
+                    login: "/", // unused now, but required by your plugin’s shape
+                    callback: "/auth/callback",
+                    include: ["/admin/**"], // only guard these
+                    exclude: [
+                        "/",
+                        "/roster",
+                        "/donate",
+                        "/contact",
+                        "/auth/callback",
+                    ],
+                    cookieRedirect: false,
+                    saveRedirectToCookie: false,
+                },
+            },
+        },
+    },
+
+    app: {
+        head: {
+            link: [
+                {
+                    rel: "stylesheet",
+                    href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css",
+                },
+            ],
+        },
+    },
 });
