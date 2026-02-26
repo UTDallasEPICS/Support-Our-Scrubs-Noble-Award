8653e0bc344a49f1f1f3ca10d11987bffda45bd6
Aagam Shah <shahaagam04@gmail.com>
2026-02-26 17:16:57 -0600

    feat: update database migrations and seed script for user and Nominator normalization

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,17 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 async function main() {
-    await prisma.admin.upsert({
-        where: { email: process.env.SEED_EMAIL! },
-        update: {},
-        create: {
-            email: process.env.SEED_EMAIL!,
-            Position: "admin",
-        }
-    });
-
-    console.log(`A seed admin account has been created with the email ${process.env.SEED_EMAIL}`);
-    console.log("In order to login, register for an account using this email and click the link it sends");    
+    
 }
 
 main().catch((err) => {
