8b5a97e0afd433713c4c58c052f258e65e8f1cfb
Aagam Shah <shahaagam04@gmail.com>
2026-04-10 08:51:52 -0500

    feat: added role based auth guards

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,9 +1,27 @@
 import { authClient } from "~/shared/auth-client";
+
 export default defineNuxtRouteMiddleware(async (to, from) => {
-	const { data: session } = await authClient.useSession(useFetch); 
-	if (!session.value) {
-		if (to.path === "/admin") {
+	const { data: session } = await authClient.useSession(useFetch);
+
+	if (!session.value?.user?.email) {
+		if (to.path === "/admin" || to.path === "/nominator/myNominees" || to.path === "/nominator/edit") {
 			return navigateTo("/");
 		}
+		return;
+	}
+
+	const { role } : { role: 'admin' | 'nominator' | 'nominee' } = await $fetch(
+		"/api/checkEmail", 
+		{ method: "POST", body: { email: session.value.user.email } }
+	);
+
+	// guard Admin Pages if user is not an admin
+	if (to.path === "/admin" && role !== 'admin') {
+		return navigateTo("/");
+	}
+
+	// guard My Nominees Page if user is not a nominator
+	if ((to.path === "/nominator/myNominees" || to.path === "/nominator/edit") && role !== 'nominator') {
+		return navigateTo("/");
 	}
 });
