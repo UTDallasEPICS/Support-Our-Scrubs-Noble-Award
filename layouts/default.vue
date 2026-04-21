c4500ef14356d2484729a6dd87640a615d708afa
Aagam Shah <shahaagam04@gmail.com>
2026-04-21 13:38:04 -0500

    feat: default layouts

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import Navbar from '~/components/Navbar.vue'
+import MyLogin from '~/components/MyLogin.vue'
+import { useLoginModal } from '~/composables/useLoginModal'
+
+const { showLogin, open, close } = useLoginModal()
+const route = useRoute()
+
+onMounted(() => {
+  if (route.query.login === 'true') open()
+})
+</script>
+
+<template>
+  <Navbar @open-login="open" />
+  <slot />
+  <Teleport to="body">
+    <MyLogin v-if="showLogin" @close="close" />
+  </Teleport>
+</template>
