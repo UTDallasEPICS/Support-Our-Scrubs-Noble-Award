7fc848bf858491acb975ee03a0acd7b731e859d9
Aagam Shah <shahaagam04@gmail.com>
2026-04-21 14:21:17 -0500

    feat: add theme.css to remove repetitive style scripts nuxt config auto imports the theme.css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 export default defineNuxtConfig({
     compatibilityDate: "2024-04-03",
     devtools: { enabled: true },
-    css: ["~/assets/css/main.css"],
+    css: ["~/assets/css/main.css", "~/assets/css/theme.css"],
     imports: { autoImport: true },
     modules: ["nuxt-security"],
 
